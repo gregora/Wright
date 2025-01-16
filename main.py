ec6eebe3d62a88967b49d356e48e0d91c0fb1a81
Gregor Alič <gregor372@gmail.com>
2025-01-16 13:28:01 +0100

    Add animation using vedo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 from math import cos, sin, pi
 import matplotlib.pyplot as plt
 
+from vedo import *
 from misc import *
 
 import tqdm
@@ -19,9 +20,24 @@ velocities = []
 airframe.v_i[0, 0] = 10
 
 
-airframe.attitude[0, 0] = 0.0
+airframe.attitude[0, 0] = 0.1
 airframe.attitude[1, 0] = -0.06
-airframe.attitude[2, 0] = 0.1
+airframe.attitude[2, 0] = 0.0
+
+mesh = Mesh("CAD/plane.obj")
+vedo_plt = Plotter(bg='beige', bg2='lb', axes=0, offscreen=True, interactive=1)
+# show axes
+
+vedo_plt += mesh
+vedo_plt += __doc__
+
+video = Video("video.mp4", duration=60, backend='ffmpeg') # backend='opencv'
+
+cam1= dict(pos=(5.805, 17.34, -0.8418),
+           focalPoint=(3.133, 1.506, -3.132),
+           viewup=(-0.3099, 0.1871, -0.9322),
+           distance=16.22,
+           clippingRange=(12.35, 21.13))
 
 
 dt = 0.001
@@ -36,6 +52,24 @@ for i in tqdm.tqdm(range(N)):
     attitudes.append(airframe.attitude.copy())
     positions.append(airframe.x_i.copy())
     velocities.append(airframe.v_i.copy())
+
+    if i % 40 == 0:
+        mesh.rotate_x(airframe.attitude[0, 0] * 180 / pi)
+        mesh.rotate_z(airframe.attitude[1, 0] * 180 / pi)
+        mesh.rotate_y(airframe.attitude[2, 0] * 180 / pi)
+
+        vedo_plt.show()
+        video.add_frame()
+
+        mesh.rotate_y(-airframe.attitude[2, 0] * 180 / pi)
+        mesh.rotate_z(-airframe.attitude[1, 0] * 180 / pi)
+        mesh.rotate_x(-airframe.attitude[0, 0] * 180 / pi)
+
+
+#video.action(cameras=[cam1])
+video.close()
+
+
 
 attitudes = np.array(attitudes)
 positions = np.array(positions)
@@ -67,5 +101,5 @@ plt.plot(T, velocities[:, 2], label="Vz")
 
 plt.legend()
 
-
 plt.show()
+
