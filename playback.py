e39da51a5dc6f87e12be9852be945834bf5c34aa
gregora <gregor372@gmail.com>
2025-02-27 20:57:53 +0100

    Some fixes

--- playback.py
+++ playback.py
@@ -63,17 +63,16 @@ while True:
     x_gps_prev = x_gps
     x_gps = np.array([[latitude], [longitude], [-altitude]]) # x points to north, y points to east, z points down
 
-    eul_gyro = np.array([[yaw + yaw_offset], [-pitch], [-roll]])
-    eul = np.array([[yaw + yaw_offset], [pitch], [roll]])
+    eul_gyro = np.array([[yaw], [-pitch], [-roll]])
+    eul = np.array([[-yaw], [pitch], [roll]])
 
     R = ZYX2R(eul_gyro*pi/180)
     a_i = R.T @ np.array([[a_bx], [a_by], [a_bz]]) # body frame acceleration to inertial frame
     
-    a_i[1, 0] = -a_i[1, 0] # y is inverted in bno055
-    a_i[2, 0] = -a_i[2, 0] # z is inverted in bno055
+    #a_i[1, 0] = -a_i[1, 0] # y is inverted in bno055
+    #a_i[2, 0] = -a_i[2, 0] # z is inverted in bno055
 
     print(x)
-    print(x_gps)
 
     if x.all() == 0.0:
         x = x_gps
@@ -82,32 +81,33 @@ while True:
     x = x + dt * v_i
     v_i = v_i + dt * a_i
 
-    if x_gps_prev is not None and x_gps_prev.all() != x_gps.all():
-        Fk = np.array([
+    Fk = np.array([
         [1, 0, 0, dt,  0,  0],
         [0, 1, 0,  0, dt,  0],
         [0, 0, 1,  0,  0, dt],
         [0, 0, 0,  1,  0,  0],
         [0, 0, 0,  0,  1,  0],
         [0, 0, 0,  0,  0,  1] 
-        ])
+    ])
 
-        Hk = np.array([
-            [1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0]
-        ])
+    Hk = np.array([
+        [1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0]
+    ])
 
-        Qk = np.diag([100, 100, 1000, 10, 10, 100])*10
-        Rk = np.eye(3) * 0.01
+    Qk = np.diag([100, 100, 100, 100, 100, 100])
+    Rk = np.eye(3) * 10
 
-        Pk = Fk @ Pk @ Fk.T + Qk
 
+    Pk = Fk @ Pk @ Fk.T + Qk
+
+    if x_gps_prev is not None and x_gps_prev.all() != x_gps.all():
         yk = x_gps - x
         Sk = Hk @ Pk @ Hk.T + Rk
         Kk = Pk @ Hk.T @ np.linalg.inv(Sk)
-        x = x + Kk[:3, :] @ yk
-        v_i = v_i + Kk[3:, :] @ yk
+        #x = x + Kk[:3, :] @ yk
+        #v_i = v_i + Kk[3:, :] @ yk
         Pk = (np.eye(6) - Kk @ Hk) @ Pk
 
     x_hist = x.copy()
